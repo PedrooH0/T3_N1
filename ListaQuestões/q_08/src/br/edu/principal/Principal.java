d37e2f1bc157bc8fd843d39e8d2ec91aa1dd702b
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2023-04-16 18:02:17 -0300

    Questão

--- ListaQuestões/q_08/src/br/edu/principal/Principal.java
+++ ListaQuestões/q_08/src/br/edu/principal/Principal.java
@@ -7,41 +7,77 @@ public class Principal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); 
 		char turno = 0, categoria = 0;
-		double sal_min, sal_inicial, aux, sal_final;
-		double cont, codigo, nht, valor;
-		for (cont = 0; cont <= 10; cont++) {
+		double sal_min = 150, sal_inicial, aux, sal_final;
+		double nht, valor = 0;
+		int codigo, cont;
+		
+		for (cont = 1; cont <= 10; cont++) {
 			
 			System.out.println("Digite a letra (Maiuscula) correpodente ao seu Turno: "
 					+ "\n(M) - Matutinno | (V) - Vespertino | (N) - Noturno ");
 			turno = sc.next().toUpperCase().charAt(0);
 			
 			System.out.println("Digite a letra (Maiuscula) correspondente a sua Categoria: "
-		    		+ "\n(O) - Operário | (G) - Gerente ");
+		    		+ "\n(O) - OperÃ¡rio | (G) - Gerente ");
 			
 		    categoria = sc.next().toUpperCase().charAt(0);
 			
-			System.out.println("Digite o seu código empresarial");
-			codigo = sc.nextDouble();
+			System.out.println("Digite o seu cÃ³digo empresarial");
+			codigo = sc.nextInt();
 			
-			System.out.println("Digite o número de horas trabalhadas");
+			System.out.println("Digite o nÃºmero de horas trabalhadas");
 			nht = sc.nextDouble();
 			
 			
 			while (turno != 'M' & turno != 'V' & turno != 'N') {
-				System.out.println("Opção inválida, tete novamente");
+				System.out.println("OpÃ§Ã£o invÃ¡lida, tente novamente");
 				System.out.println("Digite a letra (Maiuscula) correpodente ao seu Turno: "
 						+ "\n(M) - Matutinno | (V) - Vespertino | (N) - Noturno ");
 				turno = sc.next().toUpperCase().charAt(0);
 			}
 			
 			while (categoria != 'G' & categoria != 'O') {
-				System.out.println("Opção inválida, tete novamente");
+				System.out.println("OpÃ§Ã£o invÃ¡lida, tente novamente");
 				System.out.println("Digite a letra (Maiuscula) correspondente a sua Categoria: "
-			    		+ "\n(O) - Operário | (G) - Gerente ");
+			    		+ "\n(O) - OperÃ¡rio | (G) - Gerente ");
 			    categoria = sc.next().toUpperCase().charAt(0);
 			}
 		    
-		    
+			if (categoria == 'G') {
+				if(turno == 'N') {
+					valor = (18/100.0) * sal_min;
+				}
+				else {
+					valor = (15/100.0) * sal_min;
+				}
+			}
+			else {
+				if(turno == 'N') {
+					valor = (13/100.0) * sal_min;
+				}
+				else {
+					valor = (10/100.0) * sal_min;
+				}
+			}
+			
+			sal_inicial = nht * valor;
+			
+			if (sal_inicial <= 300) {
+				aux = (20/100.0) * sal_inicial;
+			}
+			
+			else if (sal_inicial < 600) {
+				aux = (15/100.0) * sal_inicial;
+			}
+			
+			else {
+				aux = (5/100.0) * sal_inicial;
+			}
+			
+			sal_final = sal_inicial + aux;
+			
+			System.out.println("\nTabela " + cont +"\nCÃ³digo: " + codigo + "\nHoras trabalhadas: " + nht + "\nValor: " + valor + "\nSalÃ¡rio inicial: " + sal_inicial + "\nAuxÃ­lio: " + aux + "\nSalÃ¡rio Final: " + sal_final);
+		    System.out.println();
 		    
 		    
 
